2813212441e595409254a186adee2c0056a1eac9
frontend <gabriellennon@wizsolucoes.com.br>
2022-08-14 16:38:51 -0300

    implement modal component and request to back-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { UsersService } from 'src/app/core/services/users/users.service';
+import { IUserData } from 'src/app/shared/models/dataUser';
 import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
@@ -8,10 +10,35 @@ import { ModalComponent } from '../../components/modal/modal.component';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  loading = true;
+  error = false;
+  dataUser: IUserData[] = [];
 
-  constructor(private matDialog: MatDialog) { }
+  constructor(
+    private matDialog: MatDialog,
+    private userService: UsersService
+  ) { }
 
   ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(){
+    try {
+      this.userService.getUsers('10').subscribe({
+        next: res => this.dataUser = res.results,
+        error: (err) => {
+          console.log(err);
+          this.loading = false;
+          this.error = true;
+        },
+        complete: () => {
+          this.loading = false;
+        }
+      })
+    } catch (error) {
+        console.log(error);
+    }
   }
 
   openModal() {
